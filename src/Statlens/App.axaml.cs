91ab9bc3f217102ecac2cf5465ea757fcb288e66
padparadscho <contact@padparadscho.com>
2026-08-01 06:00:34 -0300

    feat(app): add tray icon for show, hide, and exit

--- src/Statlens/App.axaml.cs
+++ src/Statlens/App.axaml.cs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Statlens.ViewModels;
@@ -11,18 +12,51 @@ namespace Statlens;
 
 public partial class App : Application
 {
+    private MainWindow? _mainWindow;
+
     public override void Initialize() => AvaloniaXamlLoader.Load(this);
 
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
+            _mainWindow = new MainWindow
             {
                 DataContext = new MainViewModel(),
             };
+
+            desktop.MainWindow = _mainWindow;
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private void OnTrayIconClicked(object? sender, System.EventArgs eventArgs) => ToggleMainWindowVisibility();
+
+    private void OnToggleVisibilityClicked(object? sender, System.EventArgs eventArgs) => ToggleMainWindowVisibility();
+
+    private void OnExitClicked(object? sender, System.EventArgs eventArgs)
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.Shutdown();
+        }
+    }
+
+    private void ToggleMainWindowVisibility()
+    {
+        if (_mainWindow is null)
+        {
+            return;
+        }
+
+        if (_mainWindow.IsVisible)
+        {
+            _mainWindow.Hide();
+        }
+        else
+        {
+            _mainWindow.Show();
+        }
     }
 }
